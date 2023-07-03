bca3443b12b16fa5b06af7b6d8d1a18d2132e875
Alex Us <nightcroller@yandex.ru>
2023-07-03 07:58:09 +0300

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import { Router } from '@/router'
 import { RootStoreProvider } from '@/stores'
 import { GlobalStyle } from '@/styles/base'
 import './i18n'
-import 'antd/dist/reset.css'
 import 'mapbox-gl/dist/mapbox-gl.css'
 
 const container = document.getElementById('root')
