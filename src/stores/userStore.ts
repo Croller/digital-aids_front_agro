325c46e3b8b510492cf5a07444f72c3e6acd2736
Alex Us <nightcroller@yandex.ru>
2023-07-11 11:59:34 +0300

    auth fix

--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -3,8 +3,7 @@ import { request } from '@/services/request'
 import { AuthServices } from '@/services/auth'
 import { type TRule, type TUser } from '@/types/user'
 import { type TResponseData } from '@/types/http'
-import { getRedirectUrl } from '@/utils/url'
-import { getCookies, setCookies } from '@/utils/cookies'
+import { getCookies } from '@/utils/cookies'
 
 export class UserStore {
   token: string | null = null
@@ -38,29 +37,24 @@ export class UserStore {
 
   init = (): void => {
     const token = getCookies('token')
+    const [path, pathToken] = window.location.href.split('?token=')
 
     if (token) {
       this.setToken(token)
       this.authServices.fetchAuth()
-    } else if (window.location.href.split('?token=')[1]) {
-      this.setToken(window.location.href.split('?token=')[1])
+    } else if (pathToken) {
+      this.setToken(pathToken)
       this.authServices.fetchAuth()
+      window.history.pushState('', '', path)
     } else {
       const { href } = window.location
-      setCookies({ redirect: href })
       window.location.href = `https://${process.env.HOST ?? ''}/user/auth?redirect=${href}`
-      // window.location.href = `${getRedirectUrl('auth')}/user/auth`
-    }
-
-    if (window.location.href.split('?token=')[1]) {
-      window.history.pushState('', '', window.location.href.split('?token=')[0])
     }
   }
 
   setToken = (token: string | null = null): void => {
     this.token = token
     request.setToken(this.token)
-    setCookies({ token: this.token })
   }
 
   setLoading = (bool: boolean): void => {
@@ -77,14 +71,8 @@ export class UserStore {
 
   setResponse = (resp: TResponseData): void => {
     const { href } = window.location
-    const redirect = getCookies('redirect')
     if (resp.token) {
       this.setToken(resp.token)
-
-      if (redirect) {
-        window.location.href = redirect
-        setCookies({ redirect: null })
-      }
     }
     if (resp.user) this.user = resp.user
     if (resp.rules) this.rules = resp.rules
@@ -92,8 +80,7 @@ export class UserStore {
     if (resp.error) {
       this.error = resp.error
       if (resp.error.key === 'authError') {
-        setCookies({ redirect: href })
-        window.location.href = `${getRedirectUrl('auth')}/user/auth`
+        window.location.href = `https://${process.env.HOST ?? ''}/user/auth?redirect=${href}`
         this.setToken(null)
       }
     }
